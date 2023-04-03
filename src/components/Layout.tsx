7af535ca2607414708b6fea7e05f7811e9817c6e
Rafik <rafikshmoury@gmail.com>
2023-04-03 16:32:52 +0300

    fix: hide scrollbar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import { type PropsWithChildren } from "react";
 export const PageLayout = ({ children }: PropsWithChildren) => {
   return (
     <main className="flex h-screen justify-center">
-      <div className="h-full w-full overflow-y-auto border-x border-slate-400 md:max-w-2xl">
+      <div className="scrollbar-hide h-full w-full overflow-y-auto border-x border-slate-400 md:max-w-2xl">
         {children}
       </div>
     </main>
